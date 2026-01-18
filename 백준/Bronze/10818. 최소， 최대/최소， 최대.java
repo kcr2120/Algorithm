58544c0c2e5cf16bd7c58b8d94098ccdcb8c43e7
kcr2120 <kcr2120@gmail.com>
2026-01-18 23:25:24 +0900

    [Bronze III] Title: 최소, 최대, Time: 1532 ms, Memory: 319540 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            if (arr[i] < min) { // 최솟값
+                min = arr[i];
+            }
+            if (arr[i] > max) { // 최댓값
+                max = arr[i];
+            }
+        }
+
+        System.out.println(min + " " + max);
+    }
+}
