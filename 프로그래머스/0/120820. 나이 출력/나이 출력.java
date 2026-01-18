9e53eab7e89ac3db4327b339192b797d2b9d98ee
kcr2120 <kcr2120@gmail.com>
2026-01-18 19:05:55 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void main(String[] args){
+        System.out.println(solution(40));
+    }
+    public static int solution(int age) {
+        int answer = 0;
+        answer = 2023 - age;
+        return answer;
+    }
+}
