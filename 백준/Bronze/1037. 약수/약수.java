5c9ab933257e4502c1339e29232dd8ffb62167bc
kcr2120 <kcr2120@gmail.com>
2026-04-09 02:02:42 +0900

    [Bronze I] Title: 약수, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+
+        int[] arr = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[0] * arr[arr.length - 1]);
+    }
+}
