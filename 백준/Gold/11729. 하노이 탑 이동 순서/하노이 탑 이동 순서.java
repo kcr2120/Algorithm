2c3a5da6bc569ad3587ca8bdd0a0db7f390db18a
kcr2120 <kcr2120@gmail.com>
2026-04-13 18:08:46 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 540 ms, Memory: 47396 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        BigInteger count = new BigInteger("2").pow(N).subtract(BigInteger.ONE);
+        System.out.println(count);
+
+        if (N <= 20) {
+            hanoi(N,1,2,3);
+            System.out.println(sb);
+        }
+    }
+    static void hanoi(int n, int from, int via, int to) {
+        if (n== 1) {
+            sb.append(from).append(" ").append(to).append("\n");
+            return;
+        }
+        hanoi(n-1, from, to , via);
+        sb.append(from).append(" ").append(to).append("\n");
+        hanoi(n-1, via, from, to);
+    }
+}
