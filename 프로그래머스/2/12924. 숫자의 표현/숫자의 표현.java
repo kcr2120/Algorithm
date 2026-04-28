d974b93786f8f282bd3e5fc21dc92d04279a4653
kcr2120 <kcr2120@gmail.com>
2026-04-29 01:22:31 +0900

    [level 2] Title: 숫자의 표현, Time: 0.10 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i += 2) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
